97c26265191be861b8b59e4ed311bd149550bdb0
Bannik <me@bannik.eu>
2011-08-03 14:34:32 +0300

    Edited models/database.php via GitHub

--- models/database.php
+++ models/database.php
@@ -3,7 +3,7 @@ function db($mysql){
     $res = mysql_query( $mysql ) or die( mysql_error() );
     return $res;
 }
-mysql_connect("localhost","bannik","1234");
+mysql_connect("localhost","bannik","*******");
 mysql_select_db("bannik");
 mysql_query( "SET NAMES UTF8" );
 ?>           
